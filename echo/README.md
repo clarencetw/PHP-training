47ca3c985dc98cfd2c2d77e82a3471f6ed95b2d8
Clarence <mr.lin.clarence@gmail.com>
2021-01-18 16:16:29 +0800

    Add echo example

---
+++ echo/README.md
@@ -0,0 +1,9 @@
+# Echo PHP
+
+請到資料夾下開啟 HTTP Server
+
+```
+php -S localhost:8000
+```
+
+開啟網頁 http://localhost:8000
---
+++ echo/echo.php
@@ -0,0 +1,7 @@
+<?php
+  if(isset($_POST['pet'])) {
+    echo $_POST['pet'];
+  } else {
+    echo '請輸入寵物';
+  }
+?>
